97ccdd5a61096b72161b8358759ff3df2dab961d
SMRsaimon <smrsaimon960@gmail.com>
2021-09-08 00:20:46 -0700

    create context

---
+++ src/Contexts/ViewContextProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext, useReducer } from 'react';
+import { initialState } from '../state/initialState';
+import { reducer } from '../state/reducer';
+export const ViewContext = createContext();
+const ViewContextProvider = ({ children }) => {
+  // const [changeViewStyle, setChangeViewStyle] = useState('list');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <ViewContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ViewContext.Provider>
+  );
+};
+
+export default ViewContextProvider;
